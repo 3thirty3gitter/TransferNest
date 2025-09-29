03933005dc3490a6fcdc1b5e533ab0cdede47d10
3Thirty3 Ltd. <threethirtythreeprinting@gmail.com>
2025-09-29 17:08:42 +0000

    STRICT MODE: You are a world-class Firebase developer, programmer, and d

--- src/lib/nesting-algorithm.ts
+++ src/lib/nesting-algorithm.ts
@@ -1,10 +1,8 @@
-
 'use client';
 
 import type { NestedLayout } from '@/app/schema';
 
 // ---------- Types ----------
-// Basic rectangle interface
 interface Rectangle {
   x: number;
   y: number;
@@ -16,7 +14,6 @@ interface Rectangle {
 interface Score {
   value: number;
 }
-
 
 /**
  * MaxRectsBinPack
@@ -59,7 +56,19 @@ class MaxRectsBinPack {
       case 'BestShortSideFit':
         newRect = this.findPositionForNewNodeBestShortSideFit(width, height, score1, score2);
         break;
-      // Other heuristics are available but BSSF is the most recommended and robust for this use case.
+      // Other heuristics from the provided source are included for completeness
+      case 'BestLongSideFit':
+        newRect = this.findPositionForNewNodeBestLongSideFit(width, height, score2, score1);
+        break;
+      case 'BestAreaFit':
+        newRect = this.findPositionForNewNodeBestAreaFit(width, height, score1, score2);
+        break;
+      case 'BottomLeftRule':
+        newRect = this.findPositionForNewNodeBottomLeft(width, height, score1, score2);
+        break;
+      case 'ContactPointRule':
+        newRect = this.findPositionForNewNodeContactPoint(width, height, score1);
+        break;
     }
 
     if (newRect.height === 0) {
@@ -81,8 +90,8 @@ class MaxRectsBinPack {
     for (let i = 0; i < this.freeRectangles.length; ++i) {
       // Try to place the rectangle in upright (non-flipped) orientation
       if (this.freeRectangles[i].width >= width && this.freeRectangles[i].height >= height) {
-        const leftoverHoriz = this.freeRectangles[i].width - width;
-        const leftoverVert = this.freeRectangles[i].height - height;
+        const leftoverHoriz = Math.abs(this.freeRectangles[i].width - width);
+        const leftoverVert = Math.abs(this.freeRectangles[i].height - height);
         const shortSide = Math.min(leftoverHoriz, leftoverVert);
         const longSide = Math.max(leftoverHoriz, leftoverVert);
 
@@ -97,8 +106,8 @@ class MaxRectsBinPack {
       }
 
       if (this.allowRotations && this.freeRectangles[i].width >= height && this.freeRectangles[i].height >= width) {
-        const leftoverHoriz = this.freeRectangles[i].width - height;
-        const leftoverVert = this.freeRectangles[i].height - width;
+        const leftoverHoriz = Math.abs(this.freeRectangles[i].width - height);
+        const leftoverVert = Math.abs(this.freeRectangles[i].height - width);
         const shortSide = Math.min(leftoverHoriz, leftoverVert);
         const longSide = Math.max(leftoverHoriz, leftoverVert);
 
@@ -114,6 +123,156 @@ class MaxRectsBinPack {
     }
     return bestNode;
   }
+  
+    private findPositionForNewNodeBottomLeft(width: number, height: number, bestY: Score, bestX: Score): Rectangle {
+        let bestNode: Rectangle = { x: 0, y: 0, width: 0, height: 0 };
+        bestY.value = Number.MAX_VALUE;
+        bestX.value = Number.MAX_VALUE;
+
+        for (let i = 0; i < this.freeRectangles.length; ++i) {
+            // Try to place the rectangle in upright orientation
+            if (this.freeRectangles[i].width >= width && this.freeRectangles[i].height >= height) {
+                const topSideY = this.freeRectangles[i].y + height;
+                if (topSideY < bestY.value || (topSideY === bestY.value && this.freeRectangles[i].x < bestX.value)) {
+                    bestNode.x = this.freeRectangles[i].x;
+                    bestNode.y = this.freeRectangles[i].y;
+                    bestNode.width = width;
+                    bestNode.height = height;
+                    bestY.value = topSideY;
+                    bestX.value = this.freeRectangles[i].x;
+                }
+            }
+
+            if (this.allowRotations && this.freeRectangles[i].width >= height && this.freeRectangles[i].height >= width) {
+                const topSideY = this.freeRectangles[i].y + width;
+                if (topSideY < bestY.value || (topSideY === bestY.value && this.freeRectangles[i].x < bestX.value)) {
+                    bestNode.x = this.freeRectangles[i].x;
+                    bestNode.y = this.freeRectangles[i].y;
+                    bestNode.width = height;
+                    bestNode.height = width;
+                    bestY.value = topSideY;
+                    bestX.value = this.freeRectangles[i].x;
+                }
+            }
+        }
+        return bestNode;
+    }
+
+    private findPositionForNewNodeBestAreaFit(width: number, height: number, bestAreaFit: Score, bestShortSideFit: Score): Rectangle {
+        let bestNode: Rectangle = { x: 0, y: 0, width: 0, height: 0 };
+        bestAreaFit.value = Number.MAX_VALUE;
+        bestShortSideFit.value = Number.MAX_VALUE;
+
+        for (let i = 0; i < this.freeRectangles.length; ++i) {
+            const areaFit = this.freeRectangles[i].width * this.freeRectangles[i].height - width * height;
+
+            if (this.freeRectangles[i].width >= width && this.freeRectangles[i].height >= height) {
+                const leftoverHoriz = Math.abs(this.freeRectangles[i].width - width);
+                const leftoverVert = Math.abs(this.freeRectangles[i].height - height);
+                const shortSideFit = Math.min(leftoverHoriz, leftoverVert);
+
+                if (areaFit < bestAreaFit.value || (areaFit === bestAreaFit.value && shortSideFit < bestShortSideFit.value)) {
+                    bestNode.x = this.freeRectangles[i].x;
+                    bestNode.y = this.freeRectangles[i].y;
+                    bestNode.width = width;
+                    bestNode.height = height;
+                    bestShortSideFit.value = shortSideFit;
+                    bestAreaFit.value = areaFit;
+                }
+            }
+
+            if (this.allowRotations && this.freeRectangles[i].width >= height && this.freeRectangles[i].height >= width) {
+                const leftoverHoriz = Math.abs(this.freeRectangles[i].width - height);
+                const leftoverVert = Math.abs(this.freeRectangles[i].height - width);
+                const shortSideFit = Math.min(leftoverHoriz, leftoverVert);
+
+                if (areaFit < bestAreaFit.value || (areaFit === bestAreaFit.value && shortSideFit < bestShortSideFit.value)) {
+                    bestNode.x = this.freeRectangles[i].x;
+                    bestNode.y = this.freeRectangles[i].y;
+                    bestNode.width = height;
+                    bestNode.height = width;
+                    bestShortSideFit.value = shortSideFit;
+                    bestAreaFit.value = areaFit;
+                }
+            }
+        }
+        return bestNode;
+    }
+
+    private findPositionForNewNodeContactPoint(width: number, height: number, contactRating: Score): Rectangle {
+        let bestNode: Rectangle = { x: 0, y: 0, width: 0, height: 0 };
+        contactRating.value = -1;
+
+        for (let i = 0; i < this.freeRectangles.length; ++i) {
+            if (this.freeRectangles[i].width >= width && this.freeRectangles[i].height >= height) {
+                const rectX = this.freeRectangles[i].x;
+                const rectY = this.freeRectangles[i].y;
+                const score = this.contactPointScore(rectX, rectY, width, height);
+                if (score > contactRating.value) {
+                    bestNode.x = rectX;
+                    bestNode.y = rectY;
+                    bestNode.width = width;
+                    bestNode.height = height;
+                    contactRating.value = score;
+                }
+            }
+
+            if (this.allowRotations && this.freeRectangles[i].width >= height && this.freeRectangles[i].height >= width) {
+                const rectX = this.freeRectangles[i].x;
+                const rectY = this.freeRectangles[i].y;
+                const score = this.contactPointScore(rectX, rectY, height, width);
+                if (score > contactRating.value) {
+                    bestNode.x = rectX;
+                    bestNode.y = rectY;
+                    bestNode.width = height;
+                    bestNode.height = width;
+                    contactRating.value = score;
+                }
+            }
+        }
+        return bestNode;
+    }
+    
+    private findPositionForNewNodeBestLongSideFit(width: number, height: number, bestShortSide: Score, bestLongSide: Score): Rectangle {
+        let bestNode: Rectangle = { x: 0, y: 0, width: 0, height: 0 };
+        bestLongSide.value = Number.MAX_VALUE;
+        bestShortSide.value = Number.MAX_VALUE;
+
+        for (let i = 0; i < this.freeRectangles.length; ++i) {
+            if (this.freeRectangles[i].width >= width && this.freeRectangles[i].height >= height) {
+                const leftoverHoriz = Math.abs(this.freeRectangles[i].width - width);
+                const leftoverVert = Math.abs(this.freeRectangles[i].height - height);
+                const shortSide = Math.min(leftoverHoriz, leftoverVert);
+                const longSide = Math.max(leftoverHoriz, leftoverVert);
+
+                if (longSide < bestLongSide.value || (longSide === bestLongSide.value && shortSide < bestShortSide.value)) {
+                    bestNode.x = this.freeRectangles[i].x;
+                    bestNode.y = this.freeRectangles[i].y;
+                    bestNode.width = width;
+                    bestNode.height = height;
+                    bestShortSide.value = shortSide;
+                    bestLongSide.value = longSide;
+                }
+            }
+
+            if (this.allowRotations && this.freeRectangles[i].width >= height && this.freeRectangles[i].height >= width) {
+                const leftoverHoriz = Math.abs(this.freeRectangles[i].width - height);
+                const leftoverVert = Math.abs(this.freeRectangles[i].height - width);
+                const shortSide = Math.min(leftoverHoriz, leftoverVert);
+                const longSide = Math.max(leftoverHoriz, leftoverVert);
+
+                if (longSide < bestLongSide.value || (longSide === bestLongSide.value && shortSide < bestShortSide.value)) {
+                    bestNode.x = this.freeRectangles[i].x;
+                    bestNode.y = this.freeRectangles[i].y;
+                    bestNode.width = height;
+                    bestNode.height = width;
+                    bestShortSide.value = shortSide;
+                    bestLongSide.value = longSide;
+                }
+            }
+        }
+        return bestNode;
+    }
 
   /**
    * Place a rectangle and update free rectangles
@@ -198,6 +357,29 @@ class MaxRectsBinPack {
     }
   }
 
+    private contactPointScore(x: number, y: number, width: number, height: number): number {
+        let score = 0;
+
+        if (x === 0 || x + width === this.binWidth) score += height;
+        if (y === 0 || y + height === this.binHeight) score += width;
+
+        for (let i = 0; i < this.usedRectangles.length; i++) {
+            const rect = this.usedRectangles[i];
+            if (rect.x === x + width || rect.x + rect.width === x) {
+                score += this.commonIntervalLength(rect.y, rect.y + rect.height, y, y + height);
+            }
+            if (rect.y === y + height || rect.y + rect.height === y) {
+                score += this.commonIntervalLength(rect.x, rect.x + rect.width, x, x + width);
+            }
+        }
+        return score;
+    }
+
+    private commonIntervalLength(i1start: number, i1end: number, i2start: number, i2end: number): number {
+        if (i1end < i2start || i2end < i1start) return 0;
+        return Math.min(i1end, i2end) - Math.max(i1start, i2start);
+    }
+    
   /**
    * Check if rectangle a is contained in rectangle b
    */
@@ -213,57 +395,52 @@ export function nestImages(
   images: { id: string; url: string; width: number; height: number }[],
   sheetWidth: number
 ): { placedItems: NestedLayout; sheetLength: number } {
-
-  if (images.length === 0) {
-    return { placedItems: [], sheetLength: 0 };
-  }
   
-  // Create a deep copy of images to prevent state mutation issues between renders.
-  // This ensures the algorithm is always working with fresh, original data.
+  // Create a deep copy of the images to pack. This is CRITICAL to prevent
+  // state mutations between re-renders and sheet size changes.
   const itemsToPack = JSON.parse(JSON.stringify(images));
 
   // The packer needs a large virtual height to simulate a continuous roll.
   const VIRTUAL_SHEET_HEIGHT = 10000;
-  const packer = new MaxRectsBinPack(sheetWidth, VIRTUAL_SHEET_HEIGHT, true);
+  const packer = new MaxRectsBinPack(sheetWidth, VIRTUAL_SHEET_HEIGHT, true); // Allow rotations
 
   const margin = 0.2;
 
   // Add margin to images for packing and sort them by largest dimension (a proven heuristic).
   const processedItems = itemsToPack.map((img: any) => ({
     ...img,
-    width: img.width + margin,
-    height: img.height + margin,
+    widthWithMargin: img.width + margin,
+    heightWithMargin: img.height + margin,
   })).sort((a: any, b: any) => Math.max(b.width, b.height) - Math.max(a.width, a.height));
   
   const placedItems: NestedLayout = [];
   let maxSheetY = 0;
 
   for (const item of processedItems) {
-    // Check if the item is too wide for the sheet, even if rotated.
-    if (item.width > sheetWidth && item.height > sheetWidth) {
-        const minDim = Math.min(item.width - margin, item.height - margin);
-        throw new Error(`Image is too wide for the sheet. Item requires ${minDim.toFixed(2)}", but sheet width is only ${sheetWidth.toFixed(2)}".`);
-    }
-
-    const rect = packer.insert(item.width, item.height, 'BestShortSideFit');
+    const rect = packer.insert(item.widthWithMargin, item.heightWithMargin, 'BestShortSideFit');
+    
     if (rect) {
-      const isRotated = rect.width !== item.width;
+      // If the packed rectangle's width doesn't match the item's width (with margin), it was rotated.
+      const isRotated = rect.width !== item.widthWithMargin;
+      
       placedItems.push({
         id: item.id,
         url: item.url,
         x: rect.x + (margin / 2),
         y: rect.y + (margin / 2),
-        // CRITICAL: Assign final width/height based on rotation.
-        width: isRotated ? item.height - margin : item.width - margin,
-        height: isRotated ? item.width - margin : item.height - margin,
+        // CRITICAL: Use the original, un-margined dimensions for the final output.
+        // And swap them if the item was rotated.
+        width: isRotated ? item.height : item.width,
+        height: isRotated ? item.width : item.height,
       });
       // The final sheet length is the highest point reached by any placed rectangle's bottom edge.
       maxSheetY = Math.max(maxSheetY, rect.y + rect.height);
     } else {
-      console.warn(`Could not place item ${item.id}. This might happen if the sheet is full.`);
+        // This should not happen with a virtual height, but is a safeguard.
+        console.warn(`Could not place item ${item.id}.`);
     }
   }
 
   // Return the list of placed items and the final calculated sheet length.
-  return { placedItems: placedItems, sheetLength: maxSheetY };
+  return { placedItems, sheetLength: maxSheetY };
 }
