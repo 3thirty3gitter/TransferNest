74292b5a48e4e4bcd4db6b3b58be6a847952ee39
3Thirty3 Ltd. <threethirtythreeprinting@gmail.com>
2025-09-29 20:11:25 +0000

    STRICT MODE: You are a world-class Firebase developer, programmer, and d

--- next.config.ts
+++ next.config.ts
@@ -14,19 +14,16 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'placehold.co',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
-        port: '',
         pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
-        port: '',
         pathname: '/**',
       },
     ],
